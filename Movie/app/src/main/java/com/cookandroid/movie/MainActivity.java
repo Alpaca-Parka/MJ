528c9c591f898b7e9392d90f663ecb24087c206b
kag0330 <65347323+kag0330@users.noreply.github.com>
2023-05-21 22:56:57 +0900

    Revert "5-21 최신화"
    This reverts commit 707d75890b42a23deaab6d443fe9e8d936dd1d59.

--- Movie/app/src/main/java/com/cookandroid/movie/MainActivity.java
+++ Movie/app/src/main/java/com/cookandroid/movie/MainActivity.java
@@ -2,11 +2,13 @@ package com.cookandroid.movie;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Locale;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.Menu;
@@ -18,10 +20,12 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 import com.cookandroid.movie.helper.PermissionManager;
 
@@ -69,13 +73,13 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view,
                                         int position, long id) {
-                    String name = (String)arFiles.get(position);
+                    String Name = (String)arFiles.get(position);
 
-                    if(name.startsWith("[") && name.endsWith("]")){
-                        name = name.substring(1, name.length() - 1);
+                    if(Name.startsWith("[") && Name.endsWith("]")){
+                        Name = Name.substring(1, Name.length() - 1);
                     }
 
-                    String Path = mCurrent + "/" + name;
+                    String Path = mCurrent + "/" + Name;
                     File f = new File(Path);
                     if(f.isDirectory()){
                         mCurrent = Path;
@@ -115,17 +119,17 @@ public class MainActivity extends AppCompatActivity {
         if(files != null){
             for(int i = 0; i < files.length;i++){
                 String Path = mCurrent + "/" + files[i];
-                String name = "";
+                String Name = "";
 
                 File f = new File(Path);
                 if(f.isDirectory()){
-                    name = "[" + files[i] + "]";
+                    Name = "[" + files[i] + "]";
                 }else{
-                    name = files[i];
+                    Name = files[i];
                 }
-                                           //대소문자 구별하려면 toLowerCase() 사용하지 말 것
-                if (searchText.isEmpty() || name.toLowerCase().contains(searchText)) { // 검색어가 비어있거나 파일 이름이 검색어를 포함하면 추가
-                    arFiles.add(name);
+
+                if (searchText.isEmpty() || Name.contains(searchText)) { // 검색어가 비어있거나 파일 이름이 검색어를 포함하면 추가
+                    arFiles.add(Name);
                 }
             }
         }
@@ -142,12 +146,12 @@ public class MainActivity extends AppCompatActivity {
         mSearch.setOnActionExpandListener(new MenuItem.OnActionExpandListener(){
             @Override
             public boolean onMenuItemActionExpand(MenuItem item){
-                // 검색창 확장시
+                refreshFiles();
                 return true;
             }
             @Override
             public boolean onMenuItemActionCollapse(MenuItem item){
-                //검색창 축소시
+                refreshFiles();
                 return true;
             }
         });
@@ -158,7 +162,6 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextSubmit(String query){
-                //검색버튼 클릭시
                 searchText = query;
                 refreshFiles();
                 InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -168,7 +171,6 @@ public class MainActivity extends AppCompatActivity {
             }
             @Override
             public boolean onQueryTextChange(String newText){
-                //타이핑 칠때마다
                 searchText = newText;
                 refreshFiles();
                 return true;
